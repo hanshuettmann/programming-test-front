ec79189bf7a85cbf44fe66a9ff352421b32f1c9f
hanshuettmann <hans.huttmann1@gmail.com>
2020-08-14 18:55:55 -0300

    new sale form added

---
+++ src/Components/NewSale/NewSale.js
@@ -0,0 +1,325 @@
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_INPUTS':
+            return {
+                data: action.data
+            }
+
+        case 'SET_CLIENT':
+            return {
+                ...state,
+                client: action.data
+            }
+
+        case 'SET_PRODUCT':
+            return {
+                ...state,
+                product: action.data
+            }
+
+        case 'SET_EMPLOYEE':
+            return {
+                ...state,
+                employee: action.data
+            }
+
+        case 'SET_QUANTITY':
+            return {
+                ...state,
+                quantity: action.data
+            }
+
+        case 'SET_AMOUNT':
+            return {
+                ...state,
+                totalAmount: action.data
+            }
+
+        default:
+            return state
+    }
+}
+
+const NewSale = ({ clients, products, employees, loadSale }) => {
+    const [state, dispatch] = useReducer(reducer, {
+        client: {},
+        employee: {},
+        product: {
+            price: 0
+        },
+        quantity: 1,
+        totalAmount: 0
+    });
+
+    const inputClientOnChange = (e) => {
+        let _id = e.target.value;
+        const clientData = clients.find(client => {
+            return (
+                client._id === _id
+            )
+        })
+
+        dispatch({ type: 'SET_CLIENT', data: clientData });
+    }
+
+    const inputProductOnChange = (e) => {
+        let _id = e.target.value;
+        const productData = products.find(product => {
+            return (
+                product._id === _id
+            )
+        })
+
+        dispatch({ type: 'SET_PRODUCT', data: productData });
+    }
+
+    const inputEmployeeOnChange = (e) => {
+        let _id = e.target.value;
+        const employeeData = employees.find(employee => {
+            return (
+                employee._id === _id
+            )
+        })
+
+        dispatch({ type: 'SET_EMPLOYEE', data: employeeData });
+    }
+
+    const inputQuantityOnChange = (e) => {
+        const inputValue = e.target.value;
+        if (inputValue.length === 0) {
+            dispatch({ type: 'SET_QUANTITY', data: '' });
+        } else {
+            const quantity = isNaN(parseFloat(e.target.value)) ? 0 : parseFloat(e.target.value);
+            dispatch({ type: 'SET_QUANTITY', data: quantity });
+        }
+    }
+
+    const inputAmountOnChange = (e) => {
+        const totalAmount = parseFloat(e.target.value);
+        console.log(totalAmount);
+
+        dispatch({ type: 'SET_AMOUNT', data: totalAmount });
+    }
+
+    const addSale = (e) => {
+        e.preventDefault();
+        loadSale(state);
+    }
+
+    return (
+        <div className='container'>
+            <div className='row justify-content-center'>
+                <div className='col-12 col-md-10 text-left mb-3'>
+                    <form>
+                        <div className='form-row'>
+                            <div className='form-group col-md-6'>
+                                <label htmlFor='inputEmployee'>Empleado</label>
+                                <select
+                                    id='inputEmployee'
+                                    className='form-control'
+                                    name='employee'
+                                    onChange={inputEmployeeOnChange}
+                                >
+                                    {employees.map((employee) => {
+                                        return (
+                                            <option
+                                                key={employee._id}
+                                                value={employee._id}
+                                            >
+                                                {`${employee.name} ${employee.lastname}`}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            <div className='form-group col-md-6'>
+                                <label htmlFor='inputClient'>Cliente</label>
+                                <select
+                                    id='inputClient'
+                                    className='form-control'
+                                    name='client'
+                                    onChange={inputClientOnChange}
+                                >
+                                    {clients.map((client) => {
+                                        return (
+                                            <option
+                                                key={client._id}
+                                                value={client._id}
+                                            >
+                                                {`${client.name} ${client.lastname}`}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                        </div>
+                        <div className='form-row'>
+                            <div className='form-group col-md-6'>
+                                <label htmlFor='inputProduct'>Producto</label>
+                                <select
+                                    id='inputProduct'
+                                    className='form-control'
+                                    name='productName'
+                                    defaultValue='0'
+                                    onChange={inputProductOnChange}
+                                >
+                                    <option value='0' disabled>Seleccionar producto...</option>
+                                    {products.map((product) => {
+                                        return (
+                                            <option
+                                                key={product._id}
+                                                value={product._id}
+                                            >
+                                                {`${product.name}`}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            <div className='form-group col-md-2'>
+                                <label htmlFor='inputProductPrice'>Precio unitario</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    id='inputProductPrice'
+                                    name='productPrice'
+                                    value={'$ ' + state.product.price}
+                                    disabled
+                                />
+                            </div>
+                            <div className='form-group col-md-2'>
+                                <label htmlFor='inputQuantity'>Cantidad</label>
+                                <input
+                                    type='number'
+                                    className='form-control'
+                                    id='inputQuantity'
+                                    name='quantity'
+                                    value={state.quantity}
+                                    onChange={inputQuantityOnChange}
+                                />
+                            </div>
+                            <div className='form-group col-md-2'>
+                                <label htmlFor='inputTotalAmount'>Total en pesos</label>
+                                <input
+                                    type='text'
+                                    className='form-control'
+                                    id='inputTotalAmount'
+                                    name='totalAmount'
+                                    value={'$ ' + state.quantity * state.product.price}
+                                    onChange={inputAmountOnChange}
+                                    disabled
+                                />
+                            </div>
+                        </div>
+                        <button
+                            className='btn btn-secondary'
+                            onClick={addSale}
+                        >
+                            Agregar al carrito</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+NewSale.defaultProps = {
+    employees: [
+        {
+            _id: 'asidh1o3o123j',
+            name: 'Patricio',
+            lastname: 'Hüttmann',
+            dni: 37658563,
+            birthDate: '25/04/1994',
+            idNumber: '123456'
+        },
+        {
+            _id: 'asidh1o3asdo123j',
+            name: 'Hanna',
+            lastname: 'Hüttmann',
+            dni: 34910995,
+            birthDate: '09/01/1991',
+            idNumber: '654321'
+        },
+        {
+            _id: 'asidh22121o3o123j',
+            name: 'Francisco',
+            lastname: 'Hüttmann',
+            dni: 32493397,
+            birthDate: '20/02/1987',
+            idNumber: '123654'
+        },
+        {
+            _id: 'asid321ash1o3o123j',
+            name: 'Patricia',
+            lastname: 'Pérez García',
+            dni: 11707863,
+            birthDate: '04/02/1955',
+            idNumber: '321456'
+        }
+    ],
+    clients: [
+        {
+            _id: 'asidh1o3o123j',
+            name: 'Patricio',
+            lastname: 'Canseco',
+            dni: 37658563,
+            birthDate: '25/04/1994',
+            creditCard: '4001858525254712'
+        },
+        {
+            _id: 'asidh1o3asdo123j',
+            name: 'Hanna',
+            lastname: 'Lombardo',
+            dni: 34910995,
+            birthDate: '09/01/1991',
+            creditCard: '4001858525254712'
+        },
+        {
+            _id: 'asidh22121o3o123j',
+            name: 'Francisco',
+            lastname: 'Rodriguez',
+            dni: 32493397,
+            birthDate: '20/02/1987',
+            creditCard: '4001858525254712'
+        }
+    ],
+    products: [
+        {
+            _id: 'asidh1o3o123j',
+            name: 'Queso untable',
+            brand: 'La Serenísima',
+            price: 70.0,
+            provider: 'Juan',
+            dueDate: '25/04/1994'
+        },
+        {
+            _id: 'asidh1o3o123asdaj',
+            name: 'Leche descremada',
+            brand: 'La Serenísima',
+            price: 50.0,
+            provider: 'Marcos',
+            dueDate: '25/04/1994'
+        },
+        {
+            _id: 'asidh1o3Dadsad',
+            name: 'Cerveza Negra',
+            brand: 'Salta',
+            price: 110.0,
+            provider: 'Rodrigo',
+            dueDate: '25/04/1994'
+        },
+        {
+            _id: 'asidh1o3o2as31123j',
+            name: 'Alfajor negro',
+            brand: 'Jorgito',
+            price: 35.0,
+            provider: 'Pedro',
+            dueDate: '25/04/1994'
+        }
+    ],
+    loadSale: (data) => {
+        console.log(data)
+    }
+}
+
+export default NewSale;
--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import SalesHistory from '../SalesHistory/SalesHistory';
+//import SalesHistory from '../SalesHistory/SalesHistory';
+import NewSale from '../NewSale/NewSale';
 
 const Sales = () => {
-    return(
+    return (
         <div className='mt-3 animation-show'>
-            
+            <NewSale />
         </div>
     )
 }
